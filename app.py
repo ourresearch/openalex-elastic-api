0e66ded68987c0bdd954fbcf9d5bf575c4f94619
Casey M <caseym@gmail.com>
2021-12-08 18:27:46 -0600

    add sentry

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
+
+import sentry_sdk
 from elasticsearch_dsl import connections
 from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 import settings
 import works
@@ -21,6 +25,11 @@ def register_blueprints(app):
 
 
 def register_extensions(app):
+    sentry_sdk.init(
+        dsn=os.environ.get("SENTRY_DSN"),
+        integrations=[FlaskIntegration()],
+        traces_sample_rate=0.5,
+    )
     connections.create_connection(hosts=[settings.ES_URL], timeout=30)
     with app.test_request_context():
         spec.path(view=works.views.works)
