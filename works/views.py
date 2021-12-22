a259a075803b7d66b1a148c341e332f0b17ee2c2
Casey M <caseym@gmail.com>
2021-12-22 15:58:46 -0500

    move validation into core package

--- works/views.py
+++ works/views.py
@@ -7,11 +7,11 @@ from core.exceptions import APIError
 from core.filter import filter_records
 from core.sort import sort_records
 from core.utils import map_query_params
+from core.validate import (validate_params, validate_per_page,
+                           validate_result_size)
 from works.schemas import MessageSchema
 from works.search import group_by_records, search_records
 from works.utils import convert_group_by
-from works.validate import (validate_params, validate_per_page,
-                            validate_result_size)
 
 blueprint = Blueprint("works", __name__)
 
