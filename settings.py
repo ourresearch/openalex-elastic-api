14049e58dab40c83722e53e88807f2959266354c
Casey M <caseym@gmail.com>
2021-12-27 11:22:18 -0600

    update settings

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 ENV = os.environ.get("FLASK_ENV", "production")
-ES_URL = os.environ.get("ES_URL_PROD")
+ES_URL = os.environ.get("ES_URL_PROD", "http://127.0.0.1:9200")
 DEBUG = ENV == "development"
 JSON_SORT_KEYS = False
 SECRET_KEY = os.environ.get("SECRET_KEY")
