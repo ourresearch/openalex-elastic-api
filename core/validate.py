d2704ca8d5e7f10f5ce102a80152ecea0091347e
Casey M <caseym@gmail.com>
2021-12-31 07:50:06 -0600

    allow mailto param

--- core/validate.py
+++ core/validate.py
@@ -2,7 +2,15 @@ from core.exceptions import APIQueryParamsError
 
 
 def validate_params(request):
-    valid_params = ["filter", "group_by", "group-by-size", "page", "per-page", "sort"]
+    valid_params = [
+        "filter",
+        "group_by",
+        "group-by-size",
+        "mailto",
+        "page",
+        "per-page",
+        "sort",
+    ]
     for arg in request.args:
         if arg not in valid_params:
             raise APIQueryParamsError(
