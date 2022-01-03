99c4e2bf99b5feaabd6758fe393059b58723c6d6
Casey M <caseym@gmail.com>
2022-01-03 14:37:06 -0600

    implement entity type filter for autocomplete

--- complete/views.py
+++ complete/views.py
@@ -4,16 +4,40 @@ from elasticsearch_dsl import Search
 from flask import Blueprint, request
 
 from complete.schemas import MessageSchema
+from core.exceptions import APIQueryParamsError
 
 blueprint = Blueprint("complete", __name__)
 
 
 @blueprint.route("/autocomplete")
 def autocomplete():
+    entities_to_indeces = {
+        "author": "authors-v3",
+        "concept": "concepts-v1",
+        "institution": "institutions-v1",
+        "venue": "venues-v2",
+        "work": "works-v5-*,-*invalid-data",
+    }
+
     q = request.args.get("q")
-    s = Search(
-        index="authors-v3,concepts-v1,institutions-v1,venues-v2,works-v5-*,-*invalid-data"
-    )
+    entity_type = request.args.get("entity_type")
+
+    if entity_type and not q:
+        raise APIQueryParamsError(
+            f"Must provide a 'q' parameter when filtering by an entity type."
+        )
+
+    if entity_type:
+        try:
+            index = entities_to_indeces[entity_type]
+        except KeyError:
+            raise APIQueryParamsError(
+                f"{entity_type} is not a valid value for parameter entity_type. Valid entity_type values are: {', '.join(entities_to_indeces.keys())}."
+            )
+    else:
+        index = ",".join(entities_to_indeces.values())
+
+    s = Search(index=index)
     s = s.query("match_phrase_prefix", display_name=q)
     s = s.sort("-cited_by_count")
     response = s.execute()
