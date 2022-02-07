72e58e41b0e8daef38312968e10f4605925d2ee1
Casey M <caseym@gmail.com>
2022-02-07 07:27:55 -0700

    reorganize authors fields

--- authors/fields.py
+++ authors/fields.py
@@ -1,15 +1,15 @@
 from core.fields import OpenAlexIDField, RangeField, SearchField, TermField
 
 fields = [
-    TermField(param="display_name"),
-    SearchField(param="display_name.search"),
-    RangeField(param="cited_by_count"),
-    TermField(param="last_known_institution.country_code"),
     OpenAlexIDField(param="last_known_institution.id"),
+    OpenAlexIDField(param="x_concepts.id"),
+    RangeField(param="cited_by_count"),
+    RangeField(param="works_count"),
+    SearchField(param="display_name.search"),
+    TermField(param="display_name"),
+    TermField(param="last_known_institution.country_code"),
     TermField(param="last_known_institution.ror"),
     TermField(param="last_known_institution.type"),
-    RangeField(param="works_count"),
-    OpenAlexIDField(param="x_concepts.id"),
 ]
 
 fields_dict = {f.param: f for f in fields}
