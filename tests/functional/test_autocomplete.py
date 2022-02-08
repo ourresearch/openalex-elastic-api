5d8fd1f303ef69a66577586898db6a60d972720e
Casey M <caseym@gmail.com>
2022-02-08 07:30:48 -0700

    add autocomplete tests

---
+++ tests/functional/test_autocomplete.py
@@ -0,0 +1,31 @@
+class TestFullAutoComplete:
+    def test_full_autocomplete(self, client):
+        res = client.get("/autocomplete?q=sci")
+        json_data = res.get_json()
+        first_object = json_data["results"][0]
+        assert first_object["id"] == "https://openalex.org/C41008148"
+        assert first_object["display_name"] == "Computer science"
+        assert first_object["cited_by_count"] == 116620134
+        assert first_object["entity_type"] == "concept"
+        assert first_object["external_id"] == "https://www.wikidata.org/wiki/Q21198"
+
+        for result in json_data["results"][:25]:
+            assert "sci" in result["display_name"].lower()
+
+
+class TestWorksAutoComplete:
+    def test_works_autocomplete(self, client):
+        res = client.get("/autocomplete/works?q=list")
+        json_data = res.get_json()
+        first_object = json_data["results"][0]
+        assert first_object["id"] == "https://openalex.org/W121243"
+        assert (
+            first_object["display_name"]
+            == "Cytoskeletal Mechanics: List of Contributors"
+        )
+        assert first_object["cited_by_count"] == 71
+        assert first_object["entity_type"] == "work"
+        assert first_object["external_id"] == "https://doi.org/10.1017/cbo9780511607318"
+
+        for result in json_data["results"][:25]:
+            assert "list" in result["display_name"].lower()
