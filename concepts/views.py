b42340ecec06b0b1a78832e2943c95e6aa756f06
Casey M <caseym@gmail.com>
2022-02-16 12:39:13 -0600

    add caching to other views

--- concepts/views.py
+++ concepts/views.py
@@ -3,11 +3,16 @@ from flask import Blueprint, request
 from concepts.fields import fields_dict
 from concepts.schemas import MessageSchema
 from core.shared_view import shared_view
+from core.utils import is_cached
+from extensions import cache
 
 blueprint = Blueprint("concepts", __name__)
 
 
 @blueprint.route("/concepts")
+@cache.cached(
+    timeout=24 * 60 * 60, query_string=True, unless=lambda: not is_cached(request)
+)
 def concepts():
     index_name = "concepts-v3"
     default_sort = ["-works_count", "id"]
