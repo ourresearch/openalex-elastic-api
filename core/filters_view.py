3c790294092c418cbc301d2affa0657f6187bf79
Casey M <caseym@gmail.com>
2022-02-27 11:34:37 -0600

    refactor to count or values separately

--- core/filters_view.py
+++ core/filters_view.py
@@ -32,32 +32,64 @@ def shared_filter_view(params, fields_dict, index_name):
     idx = 0
     for meta in meta_results:
         for value in meta["values"]:
-            value["count"] = responses[idx].hits.total.value
+            # increment count if it is an OR query
+            if len(meta["values"]) > 1 and meta["is_negated"]:
+                idx = idx + 1
+                value["count"] = 0
+            elif len(meta["values"]) > 1:
+                idx = idx + 1
+                value["count"] = responses[idx].hits.total.value
+            else:
+                value["count"] = responses[idx].hits.total.value
+
             value["db_response_time_ms"] = responses[idx].took
-            idx = idx + 1
         results["filters"].append(meta)
     return results
 
 
 def filter_records_filters_view(fields_dict, filter_params, ms, index_name):
     meta_results = []
-    search_param = get_search_param(filter_params)
-    search_query = None
-    if search_param:
-        # build search query
-        for key, value in search_param.items():
-            field = get_field(fields_dict, key)
-            field.value = value
-            search_query = field.build_query()
+    s = Search()
+    s = s.extra(track_total_hits=True, size=0)
+    regular_queries = []
 
+    # first pass with no OR queries
     for filter in filter_params:
         for key, value in filter.items():
-            field = get_field(fields_dict, key)
+            if "|" in value:
+                continue
 
+            field = get_field(fields_dict, key)
             field_meta = {"key": key, "type": type(field).__name__, "values": []}
 
+            if value.startswith("!"):
+                value = value[1:]
+                field.value = value  # pass value without negation
+                field_meta["is_negated"] = True
+            else:
+                field.value = value
+                field_meta["is_negated"] = False
+            q = field.build_query()
+            field_meta["values"].append(
+                {
+                    "value": value,
+                    "display_name": set_display_name(value, field),
+                    # "url": set_url(search_param, key, value, index_name),
+                }
+            )
+            regular_queries.append(q)
+            meta_results.append(field_meta)
+    combined_query = Q("bool", must=regular_queries)
+    ms = ms.add(s.query(combined_query))
+
+    # second pass for OR queries
+    for filter in filter_params:
+        for key, value in filter.items():
             # OR queries have | in the param values
             if "|" in value:
+                field = get_field(fields_dict, key)
+                field_meta = {"key": key, "type": type(field).__name__, "values": []}
+
                 if value.startswith("!"):
                     field_meta["is_negated"] = True
                     value = value.replace("!", "")
@@ -67,31 +99,22 @@ def filter_records_filters_view(fields_dict, filter_params, ms, index_name):
                 for or_value in value.split("|"):
                     validate_or_value(or_value)
                     field.value = or_value
+                    q = field.build_query()
+
+                    s = Search()
+                    s = s.extra(track_total_hits=True, size=0)
+                    s = s.query(combined_query)
+                    s = s.query(q)
+                    ms = ms.add(s)
+
                     field_meta["values"].append(
                         {
                             "value": or_value,
                             "display_name": set_display_name(or_value, field),
-                            "url": set_url(search_param, key, or_value, index_name),
+                            # "url": set_url(search_param, key, or_value, index_name),
                         }
                     )
-                    ms = execute_ms_search(field, ms, search_query)
-            else:
-                if value.startswith("!"):
-                    value = value[1:]
-                    field.value = value  # pass value without negation
-                    field_meta["is_negated"] = True
-                else:
-                    field.value = value
-                    field_meta["is_negated"] = False
-                field_meta["values"].append(
-                    {
-                        "value": value,
-                        "display_name": set_display_name(value, field),
-                        "url": set_url(search_param, key, value, index_name),
-                    }
-                )
-                ms = execute_ms_search(field, ms, search_query)
-            meta_results.append(field_meta)
+                meta_results.append(field_meta)
     return ms, meta_results
 
 
@@ -102,29 +125,6 @@ def validate_or_value(or_value):
             f"like /works?filter=concepts.id:!C144133560|C15744967, meaning NOT (C144133560 or C15744967). Problem "
             f"value: {or_value}"
         )
-
-
-def execute_ms_search(field, ms, search_query):
-    """If a search query is present, combine it with the current filter."""
-    q = field.build_query()
-    s = Search()
-    s = s.extra(track_total_hits=True, size=0)
-    if search_query:
-        combined_query = Q("bool", must=[q, search_query])
-        ms = ms.add(s.query(combined_query))
-    else:
-        ms = ms.add(s.query(q))
-    return ms
-
-
-def get_search_param(filter_params):
-    """Find and return a search param."""
-    search_param = None
-    for filter in filter_params:
-        for key, value in filter.items():
-            if key == "display_name.search" or key == "title.search":
-                search_param = {key: value}
-    return search_param
 
 
 def set_url(search_param, key, value, index_name):
