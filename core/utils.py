ce2c55c730b4a835cd5947cbc0e8506d2a72f603
Casey M <caseym@gmail.com>
2022-09-27 08:39:04 -0500

    do not show from_updated_date in errors

--- core/utils.py
+++ core/utils.py
@@ -14,6 +14,7 @@ def get_field(fields_dict, key):
         return field
     except KeyError:
         valid_fields = sorted(list(fields_dict.keys()))
+        valid_fields.remove("from_updated_date")
         raise APIQueryParamsError(
             f"{key} is not a valid field. Valid fields are underscore or hyphenated versions of: {', '.join(valid_fields)}"
         )
