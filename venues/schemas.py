6c121d302b657a118caaa1d5c57416d9271b6cf9
Casey M <caseym@gmail.com>
2022-11-09 09:44:20 -0600

    add new fields to venues schema

--- venues/schemas.py
+++ venues/schemas.py
@@ -7,12 +7,23 @@ from core.schemas import (CountsByYearSchema, GroupBySchema, MetaSchema,
 class IDsSchema(Schema):
     openalex = fields.Str()
     issn_l = fields.Str()
-    mag = fields.Str()
     issn = fields.List(fields.Str())
+    crossref = fields.Str()
+    mag = fields.Str()
+    wikidata = fields.Str()
+    fatcat = fields.Str()
 
     class Meta:
         ordered = True
         unknown = INCLUDE
+
+
+class SocietiesSchema(Schema):
+    url = fields.Str()
+    organization = fields.Str()
+
+    class Meta:
+        ordered = True
 
 
 class VenuesSchema(Schema):
@@ -21,14 +32,20 @@ class VenuesSchema(Schema):
     issn = fields.List(fields.Str())
     display_name = fields.Str()
     publisher = fields.Str()
-    type = fields.Str()
     relevance_score = fields.Method("get_relevance_score")
     works_count = fields.Int()
     cited_by_count = fields.Int()
     is_oa = fields.Bool()
     is_in_doaj = fields.Bool()
-    homepage_url = fields.Str()
     ids = fields.Nested(IDsSchema)
+    homepage_url = fields.Str()
+    apc_usd = fields.Integer()
+    country = fields.Str()
+    country_code = fields.Str()
+    societies = fields.List(fields.Nested(SocietiesSchema))
+    alternate_titles = fields.List(fields.Str())
+    abbreviated_title = fields.Str()
+    type = fields.Str()
     x_concepts = fields.List(fields.Nested(XConceptsSchema))
     counts_by_year = fields.List(fields.Nested(CountsByYearSchema))
     works_api_url = fields.Str()
