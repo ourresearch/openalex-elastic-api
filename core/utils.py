6cc23aeafb7f18ab12567f29edc9f9f90a928ea8
Casey M <caseym@gmail.com>
2023-04-24 10:56:01 -0500

    add new convenience fields

--- core/utils.py
+++ core/utils.py
@@ -102,7 +102,7 @@ def get_display_names(ids):
     else:
         index_name = get_index_name_by_id(ids[0])
     s = Search(index=index_name)
-    s = s.extra(size=200)
+    s = s.extra(size=500)
     s = s.source(["id", "display_name"])
 
     results = {}
