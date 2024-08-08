f934eac7c8335ef1e6c42ba90eb2eed64ff0d8cc
Casey M <caseym@gmail.com>
2024-08-07 21:10:39 -0500

    conver to oql format

--- oql/search.py
+++ oql/search.py
@@ -7,6 +7,7 @@ from typing import Dict, List, Optional
 import redis
 
 import settings
+from oql.query import Query
 
 redis_db = redis.Redis.from_url(settings.CACHE_REDIS_URL or "redis://localhost:6379/0")
 
@@ -18,13 +19,19 @@ class Search:
     q: str
     id: str = field(init=False)
     results: Optional[List] = field(default_factory=list)
-    results_meta: Optional[Dict] = field(default_factory=dict)
+    meta: Optional[Dict] = field(default_factory=dict)
     is_ready: bool = False
     timestamp: str = field(init=False)
 
     def __post_init__(self):
         self.id = self.id_hash()
         self.timestamp = datetime.now(timezone.utc).isoformat()
+        oql_format = get_oql_format(self.q)
+        self.meta = {
+            "q": self.q,
+            "oql": oql_format["oql"],
+            "is_valid": oql_format["is_valid"],
+        }
 
     def id_hash(self) -> str:
         return hashlib.md5(self.q.encode()).hexdigest()
@@ -39,7 +46,9 @@ class Search:
 
 def is_cache_expired(search: Dict) -> bool:
     timestamp = datetime.fromisoformat(search["timestamp"])
-    return datetime.now(timezone.utc) - timestamp > timedelta(minutes=CACHE_EXPIRATION_MINUTES)
+    return datetime.now(timezone.utc) - timestamp > timedelta(
+        minutes=CACHE_EXPIRATION_MINUTES
+    )
 
 
 def get_existing_search(id: str) -> Optional[Dict]:
@@ -48,3 +57,8 @@ def get_existing_search(id: str) -> Optional[Dict]:
         return None
     existing_search = json.loads(existing_search_json)
     return existing_search
+
+
+def get_oql_format(query_string: str) -> dict:
+    q = Query(query_string)
+    return {"is_valid": q.is_valid(), "oql": q.oql_query()}
