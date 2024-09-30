2908cf76384f60eb622665994fe091f110e09e30
Casey M <caseym@gmail.com>
2024-09-30 08:21:25 -0500

    make multiprocess

--- scripts/remove_duplicates.py
+++ scripts/remove_duplicates.py
@@ -1,36 +1,34 @@
+from multiprocessing import Pool
 from elasticsearch.helpers import bulk
 from elasticsearch_dsl import Search, connections
 from elasticsearch.exceptions import ConflictError
 from settings import ES_URL, WORKS_INDEX
 
-BATCH_SIZE = 100  # Adjust the batch size based on your needs
+BATCH_SIZE = 100
 
 
-def remove_duplicates():
-    """Find and remove duplicates from works index."""
+def remove_duplicates(work_ids):
+
     actions = []
     count = 0
 
-    with open("duplicate_record_ids_works.csv", "r") as f:
-        for line in f:
-            count += 1
-            print(f"Processing line {count}")
-            work_id = line.strip()
-            actions.extend(find_id_and_prepare_delete(work_id))
+    for work_id in work_ids:
+        count += 1
+        actions.extend(find_id_and_prepare_delete(work_id))
 
-            if len(actions) >= BATCH_SIZE:
-                bulk_delete(actions)
-                actions = []  # Reset actions after processing the batch
+        if len(actions) >= BATCH_SIZE:
+            print(f"deleting batch, line count is {count}")
+            bulk_delete(actions)
+            actions = []
 
-    # Process any remaining actions
     if actions:
         bulk_delete(actions)
 
 
 def find_id_and_prepare_delete(work_id):
-    """Find duplicates for a given work_id and prepare the delete actions."""
     s = Search(index=WORKS_INDEX)
     formatted_id = f"https://openalex.org/{work_id}"
+
     s = s.filter("term", id=formatted_id).source(False)
     s = s.sort("-@timestamp")
     response = s.execute()
@@ -38,7 +36,6 @@ def find_id_and_prepare_delete(work_id):
     actions = []
     if s.count() == 2:
         record = response.hits[1]
-        print(f"Adding duplicate record with id: {record.meta.id} in index: {record.meta.index}")
         actions.append({
             '_op_type': 'delete',
             '_index': record.meta.index,
@@ -49,17 +46,27 @@ def find_id_and_prepare_delete(work_id):
 
 
 def bulk_delete(actions):
-    """Perform bulk deletion of the actions."""
     try:
         bulk(connections.get_connection(), actions)
         print(f"Bulk deleted {len(actions)} records.")
     except ConflictError as e:
         print(f"Conflict encountered during bulk delete: {e}")
-    except Exception as e:
-        print(f"Error during bulk delete: {e}")
+
+
+def run_parallel():
+    connections.create_connection(hosts=[ES_URL], timeout=30)
+
+    with open("duplicate_record_ids_works.csv", "r") as f:
+        work_ids = [line.strip() for line in f]
+
+    print(f"Total work IDs loaded: {len(work_ids)}")
+
+    num_workers = 8
+    chunk_size = len(work_ids) // num_workers
+
+    with Pool(processes=num_workers) as pool:
+        pool.map(remove_duplicates, [work_ids[i:i + chunk_size] for i in range(0, len(work_ids), chunk_size)])
 
 
 if __name__ == "__main__":
-    # Establish Elasticsearch connection
-    connections.create_connection(hosts=[ES_URL], timeout=30)
-    remove_duplicates()
+    run_parallel()
