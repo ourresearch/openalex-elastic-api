363dab0e137ee3ed38d9a2532e73aefb4360e4fc
Casey M <caseym@gmail.com>
2025-07-02 16:32:35 -0500

    add v2 endpoints for licenses and languages

--- licenses/views.py
+++ licenses/views.py
@@ -78,6 +78,16 @@ def licenses_filters_doctrings():
     return jsonify(ret)
 
 
+@blueprint.route("/v2/licenses")
+def v2_licenses():
+    index_name = "licenses-v1"
+    default_sort = ["id"]
+    only_fields = process_only_fields(request, LicensesSchema)
+    result = shared_view(request, fields_dict, index_name, default_sort, connection='v2')
+    message_schema = MessageSchema(only=only_fields)
+    return message_schema.dump(result)
+
+
 @blueprint.route("/licenses/config")
 def licenses_config():
     return jsonify(all_entities_config["licenses"])
