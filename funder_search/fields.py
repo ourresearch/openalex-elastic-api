9509b3e2d04569cf24e32f87f1c05c21b00a43ac
Casey M <caseym@gmail.com>
2025-10-08 09:22:44 -0400

    add fulltext origin filter

--- funder_search/fields.py
+++ funder_search/fields.py
@@ -4,6 +4,7 @@ fields = [
     SearchField(param="search", custom_es_field="html"),
     SearchField(param="default.search", index="funder-search"),
     TermField(param="doi", custom_es_field="doi"),
+    TermField(param="fulltext_origin"),
 ]
 
 fields_dict = {f.param: f for f in fields}
