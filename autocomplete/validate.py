b40cc9f29bbb74c4e1fc57a162c792a0b79a6015
Casey M <caseym@gmail.com>
2025-10-23 20:29:10 -0500

    fix autocomplete with api-key bug

--- autocomplete/validate.py
+++ autocomplete/validate.py
@@ -2,7 +2,7 @@ from core.exceptions import APIQueryParamsError
 
 
 def validate_entity_autocomplete_params(request):
-    valid_params = ["q", "filter", "mailto", "search", "hide_zero", "author_hint", "data_version", "data-version"]
+    valid_params = ["q", "filter", "mailto", "search", "hide_zero", "author_hint", "data_version", "data-version", "api-key", "api_key"]
     for arg in request.args:
         if arg not in valid_params and arg != 'bypass_cache':
             raise APIQueryParamsError(
@@ -19,6 +19,8 @@ def validate_full_autocomplete_params(request):
         "q",
         "data_version",
         "data-version",
+        "api-key",
+        "api_key"
     ]
     for arg in request.args:
         if arg not in valid_params and arg != 'bypass_cache':
